a543416cdfed2e879618c0d9b5e63cf6fffa7538
wuyifan <1208097313@qq.com>
2024-08-01 01:49:18 +0800

    feat :add page

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: wuyifan 1208097313@qq.com
  * @Date: 2024-07-31 00:09:32
  * @LastEditors: wuyifan 1208097313@qq.com
- * @LastEditTime: 2024-07-31 00:29:54
+ * @LastEditTime: 2024-08-01 01:37:12
  * @FilePath: /Auto-delivery-helper/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,11 +14,21 @@ async function main() {
 
     const browser = await launch({
         headless: false, // 设置为 false 以启用可视模式
+        devtools: true,
         args: ['--disable-web-security', '--allow-file-access-from-files', '--hide-scrollbars', '--enable-gpu'],
     })
     console.log(browser);
 
-    const pages = await browser.pages();
+    const page = await (async ()=>{
+        const pages = await browser.pages();
+        return pages.length > 0 ? pages[0] : await browser.newPage();
+    })();
+
+    console.log();
+    
+
+    const url = 'https://www.zhipin.com/'
+    await page.goto(url, { waitUntil: 'networkidle2' });
 
 }
 
