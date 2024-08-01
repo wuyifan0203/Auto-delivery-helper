049eb427970b535e89ed0ac19c30566f9a6c797d
wuyifan <wuyifan@max-optics.com>
2024-08-01 17:11:01 +0800

    feat:add mask

---
+++ restart.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###
+ # @Author: wuyifan wuyifan@max-optics.com
+ # @Date: 2024-08-01 15:58:56
+ # @LastEditors: wuyifan wuyifan@max-optics.com
+ # @LastEditTime: 2024-08-01 16:08:03
+ # @FilePath: /Auto-delivery-helper/restart.sh
+ # Copyright (c) 2024 by wuyifan email: 1208097313@qq.com, All Rights Reserved.
+### 
+
+# 检查是否有旧的进程在运行
+if [ -f ./pidfile ]; then
+  old_pid=$(cat ./pidfile)
+  if ps -p $old_pid > /dev/null; then
+    echo "Killing old process $old_pid"
+    kill $old_pid
+  fi
+fi
+
+# 启动新的进程
+echo "Starting new process --------------"
+ts-node src/index.ts &
+
+# 保存新的进程 ID
+new_pid=$!
+echo $new_pid > ./pidfile
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,71 @@
 /*
  * @Author: wuyifan 1208097313@qq.com
  * @Date: 2024-07-31 00:09:32
- * @LastEditors: wuyifan 1208097313@qq.com
- * @LastEditTime: 2024-08-01 01:37:12
+ * @LastEditors: wuyifan wuyifan@max-optics.com
+ * @LastEditTime: 2024-08-01 17:09:25
  * @FilePath: /Auto-delivery-helper/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-import { launch } from 'puppeteer';
+import { launch, type Browser } from 'puppeteer';
+
+let browser: undefined | Browser;
+
+
+
 
 async function main() {
-    console.log('hollow world , i love you');
+    console.log('hollow world , i love you bb');
 
-    const browser = await launch({
+    browser = await launch({
         headless: false, // 设置为 false 以启用可视模式
         devtools: true,
         args: ['--disable-web-security', '--allow-file-access-from-files', '--hide-scrollbars', '--enable-gpu'],
+        defaultViewport: { width: 1280, height: 800 }
     })
-    console.log(browser);
 
-    const page = await (async ()=>{
+    const page = await (async () => {
         const pages = await browser.pages();
         return pages.length > 0 ? pages[0] : await browser.newPage();
     })();
 
-    console.log();
-    
 
-    const url = 'https://www.zhipin.com/'
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    const url = 'https://www.zhipin.com/';
+
+
+    const baiduUrl = 'https://www.baidu.com/';
+    await page.goto(baiduUrl, { waitUntil: 'networkidle2' });
+    await page.evaluate(() => {
+        const mask = document.createElement('div');
+        mask.style.position = 'fixed';
+        mask.style.top = '0';
+        mask.style.left = '0';
+        mask.style.width = '100%';
+        mask.style.height = '100%';
+        mask.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        mask.style.zIndex = '9999';
+        mask.id = 'full-screen-mask';
+        document.body.appendChild(mask);
+    })
 
 }
+
+// 捕获退出信号并关闭 Puppeteer 实例
+process.on('exit', async () => {
+    if (browser) {
+        console.log('Close current browser instance');
+        await browser.close();
+    }
+});
+
+process.on('SIGINT', () => {
+    process.exit();
+});
+
+process.on('SIGTERM', () => {
+    process.exit();
+});
+
+
 
 main();
