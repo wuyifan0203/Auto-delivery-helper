51505e37602f7a4baa67403050f7898fa35ffe69
wuyifan <1208097313@qq.com>
2024-09-20 01:25:00 +0800

    wip:handel env

--- puppeteer/app/boss/main.ts
+++ puppeteer/app/boss/main.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: wuyifan0203 1208097313@qq.com
  * @Date: 2024-08-30 13:05:12
- * @LastEditors: wuyifan0203 1208097313@qq.com
- * @LastEditTime: 2024-09-09 10:23:35
+ * @LastEditors: wuyifan 1208097313@qq.com
+ * @LastEditTime: 2024-09-20 01:20:11
  * @FilePath: /Auto-delivery-helper/puppeteer/app/boss/main.ts
  * Copyright (c) 2024 by wuyifan email: 1208097313@qq.com, All Rights Reserved.
  */
@@ -12,7 +12,7 @@ import { gotoWaitForRequests, preparePage } from "../../utils/preparePage";
 import { actionMap } from "./actionMap";
 import { generateGeekUrl, generateGetJobDetail } from "./url";
 import { BossState } from "./types";
-// import { cloneDeep } from "lodash-es";
+import { cloneDeep } from "lodash";
 import { state } from "./state";
 import { OptionType } from "../../script/state";
 import { random, sleep } from "../../utils";
@@ -22,7 +22,7 @@ type optionState = BossState & OptionType
 
 async function main(browser: Browser, option: any) {
 
-    const bossState = Object.assign(JSON.parse(JSON.stringify(state)), option) as optionState;
+    const bossState = Object.assign(cloneDeep(state), option) as optionState;
     const geekUrl = generateGeekUrl(bossState.geekOption);
 
     const mainPage = await (async () => {
@@ -36,30 +36,31 @@ async function main(browser: Browser, option: any) {
     console.log('finish prepare page');
 
     // await mainPage.goto('http://www.csdn.com/', { waitUntil: 'domcontentloaded',timeout: 30000 });
-    await gotoWaitForRequests(mainPage, { url: 'http://www.csdn.com/', waitUntil: 'domcontentloaded', timeout: 30000 }, () => { 
-        console.log('finish goto csdn');
-    });
-    // await sleep(5000);
+    // await gotoWaitForRequests(mainPage, { url: 'http://www.csdn.com/', waitUntil: 'domcontentloaded', timeout: 30000 }, () => { 
+    //     console.log('finish goto csdn');
+    // });
+    await sleep(5000)
 
     await mainPage.bringToFront();
+
     console.log('finish bring to front');
   
 
-    while (bossState.currentCount <= bossState.totalCount) {
+    // while (bossState.currentCount <= bossState.totalCount) {
 
-        for (let j = 0,k = bossState.untreatedJobList.length; j < k; j++) {
-            const job = bossState.untreatedJobList[j];
-            const url = generateGetJobDetail(job);
-            await gotoWaitForRequests(queryPage,{url})
-            await queryPage.bringToFront();
-            await sleep(random(2000, 4000));
-        }
+    //     for (let j = 0,k = bossState.untreatedJobList.length; j < k; j++) {
+    //         const job = bossState.untreatedJobList[j];
+    //         const url = generateGetJobDetail(job);
+    //         await gotoWaitForRequests(queryPage,{url})
+    //         await queryPage.bringToFront();
+    //         await sleep(random(2000, 4000));
+    //     }
 
-        if(state.jobList.length === state.targetCount){
-            break;
-        }
+    //     if(state.jobList.length === state.targetCount){
+    //         break;
+    //     }
 
-    }
+    // }
 
 
     console.log(`geekUrl: ${geekUrl}`);
